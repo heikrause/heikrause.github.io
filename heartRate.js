c318ff87a552bfc2f1e311e1aab3bcb237b3907f
heikrause <42677976+heikrause@users.noreply.github.com>
2018-08-25 01:16:41 +0200

    heartRate added to batterylevel

--- heartRate.js
+++ heartRate.js
@@ -5,8 +5,6 @@ class WebAnalyzer {
     this.onDisconnected = this.onDisconnected.bind(this);
   }
   
-  
-  
   async request() {
     let options = {
       "filters": [{
@@ -33,25 +31,14 @@ class WebAnalyzer {
     await characteristic.readValue();
   }
 
-  
-  
-  var myCharacteristic;
-  
-  async startHeartRateMeasurementNotifications() {
-	const service = await server.getPrimaryService(0x180D);
-    myCharacteristic = await service.getCharacteristic(0x2A37);
-    await myCharacteristic.startNotifications();
-
-    log('> Notifications started');
-    myCharacteristic.addEventListener('characteristicvaluechanged',
-        handleNotifications);
-  } catch(error) {
-    log('Argh! ' + error);
-  }
+  async startHeartRateMeasurementNotifications(listener) {
+    const service = await this.device.gatt.getPrimaryService(0x180D);
+    const characteristic = await service.getCharacteristic(0x2A37); 
+    await characteristic.startNotifications();
+    characteristic.addEventListener('characteristicvaluechanged', listener);
   }
 
   async stopHeartRateMeasurementNotifications(listener) {
-	  
     const service = await this.device.gatt.getPrimaryService(0x180D);
     const characteristic = await service.getCharacteristic(0x2A37); 
     await characteristic.stopNotifications();
@@ -68,21 +55,6 @@ class WebAnalyzer {
   onDisconnected() {
     console.log('Device is disconnected.');
   }
-  
- 
-function handleNotifications(event) {
-  let value = event.target.value;
-  let a = [];
-  // Convert raw data bytes to hex values just for the sake of showing something.
-  // In the "real" world, you'd use data.getUint8, data.getUint16 or even
-  // TextDecoder to process raw data bytes.
-  for (let i = 0; i < value.byteLength; i++) {
-    a.push('0x' + ('00' + value.getUint8(i).toString(16)).slice(-2));
-  }
-  log('> ' + a.join(' '));
-}
- 
-
 }
 
 
@@ -92,7 +64,6 @@ document.getElementById('startbutton').addEventListener('click', async event => 
   try {
     await webAnalyzer.request();
     await webAnalyzer.connect();
-	await webAnalyzer.startHeartRateMeasurementNotifications();
     /* Do something with webAnalyzer... */
   } catch(error) {
     console.log(error);
